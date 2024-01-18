3366560514fbe7b17ef3923500219a331d0a0425
Black Stack Hub <blackstackhub@gmail.com>
2024-01-18 18:49:12 +0000

    nvm

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, re_path
 from django.views.generic.base import RedirectView
 from . import views
+from django.views.generic import TemplateView
 urlpatterns = [
     path('', views.home, name='Home'),
+    path('ads.txt', TemplateView.as_view(template_name='ads.txt', content_type='text/plain')),
     re_path(r'^.*$', RedirectView.as_view(pattern_name='Home', permanent=False)),
 ]
